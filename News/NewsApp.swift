efe40f2606cddce3c675b97001c753a5d9d7186c
0x4f65 <andretan798@gmail.com>
2021-03-04 12:44:18 +1100

    Inital commit of News app

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
